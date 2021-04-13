9612d4a94420e1e05028bb9cdf4ad7ec522cbbfd
jiseung <jiseung.jung@corretto.io>
2021-04-13 17:20:30 +0900

    수정: window 예외처리

--- packages/aos-sdk-payment/src/index.ts
+++ packages/aos-sdk-payment/src/index.ts
@@ -6,7 +6,7 @@ import Client from './AosPaymentSdk';
 
 const AosPaymentSdk = new Client();
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-if (window as any) {
+if ((window as any) !== undefined) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (window as any).AosPaymentSdk = AosPaymentSdk;
 }
