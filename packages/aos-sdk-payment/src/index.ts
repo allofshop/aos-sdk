7008a4f2da27cbd0be27d5ba60f43979e3b4842f
jiseung <jiseung.jung@corretto.io>
2021-04-13 17:47:48 +0900

    수정: window

--- packages/aos-sdk-payment/src/index.ts
+++ packages/aos-sdk-payment/src/index.ts
@@ -4,11 +4,11 @@
 
 import Client from './AosPaymentSdk';
 
-const AosPaymentSdk = new Client();
-console.log(typeof window);
-if (typeof window !== 'undefined') {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  // (window as any).AosPaymentSdk = AosPaymentSdk;
-}
+// const AosPaymentSdk = new Client();
+// console.log(typeof window);
+// if (typeof window !== 'undefined') {
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   // (window as any).AosPaymentSdk = AosPaymentSdk;
+// }
 
 export default Client;
